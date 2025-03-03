4ef2cbdcb3d7673f01294c341521c638efd2de56
Benjamin de Chazeaux <dechazeauxb@gmail.com>
2025-03-03 16:23:51 +0100

    Adding Home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home]
 
   def home
+    # Logique pour la page d'accueil
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :users, only: [:new, :create]
+
+  get '/home', to: 'pages#home'
 end
