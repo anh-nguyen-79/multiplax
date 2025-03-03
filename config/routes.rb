098c03e04175902d4e554575479955f6fe65744c
Benjamin de Chazeaux <dechazeauxb@gmail.com>
2025-03-03 17:38:32 +0100

    Adding PostgresSQL

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :destroy]
 
   get '/home', to: 'pages#home'
 end
