973aed694a608a8afb1385e699dfd4a0fc2b0855
Antoine Crocherie <antoine.crocherie@gmail.com>
2025-03-04 10:57:16 +0100

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
+# Contrôleur principal dont héritent tous les autres contrôleurs
 class ApplicationController < ActionController::Base
+    # Exécute la méthode configure_permitted_parameters avant les actions des contrôleurs Devise
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Configure les paramètres autorisés pour Devise (authentification)
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
+    # Autorise le champ username lors de l'inscription
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
 
-    # For additional in app/views/devise/registrations/edit.html.erb
+    # Autorise le champ username lors de la mise à jour du compte
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  protected
+
+  # Définit la redirection après déconnexion
+  def after_sign_out_path_for(resource_or_scope)
+    root_path # Redirige vers la page d'accueil après la déconnexion
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,37 @@
+# Contrôleur qui gère les actions liées aux utilisateurs
 class UsersController < ApplicationController
   # ...
-
   def create
+    # Initialise un nouvel utilisateur avec les paramètres fournis
     @user = User.new(user_params)
     if @user.save
-      # Redirigez ou affichez un message de succès
+      # Redirige vers la page d'accueil avec un message de succès si l'enregistrement réussit
       redirect_to root_path, notice: 'Utilisateur créé avec succès.'
     else
-      # Rendre à la vue avec les erreurs
+      # Affiche à nouveau le formulaire avec les erreurs si l'enregistrement échoue
       render :new
     end
   end
 
+  # Action pour afficher le formulaire de création d'un nouvel utilisateur
   def new
+    # Initialise un nouvel objet utilisateur vide pour le formulaire
     @user = User.new
   end
 
+  # Action pour supprimer le compte de l'utilisateur actuellement connecté
+  def destroy
+    # Supprime l'utilisateur actuel de la base de données
+    current_user.destroy
+    # Redirige vers la page d'accueil avec un message de confirmation
+    redirect_to root_path, notice: 'Your account has been successfully deleted.'
+  end
 
   private
 
+  # Méthode privée pour filtrer les paramètres autorisés lors de la création/modification d'un utilisateur
   def user_params
+    # Seuls les champs email, password et password_confirmation sont autorisés
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
