dc6ac9a2be09746381b8ca5c8c1d3d8e1ec41663
Yanick Mingala <yannickmingala@gmail.com>
2025-03-06 17:10:39 +0100

    dash

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -47,7 +47,9 @@ class CarsController < ApplicationController
     @car.user = current_user if user_signed_in?
 
     if @car.save
-      redirect_to cars_path, notice: 'Car was successfully created.'
+      # redirect_to cars_path, notice: 'Car was successfully created.'
+      # redirect_to rentals_path, notice: 'Car was successfully created.'
+      redirect_to rentals_path(tab: "loueur")
     else
       # Ajouter un message flash d'erreur
       flash.now[:alert] = "There were errors in your submission. Please check the form."
@@ -64,7 +66,7 @@ class CarsController < ApplicationController
     # Vérification de sécurité: seul le propriétaire peut éditer sa voiture
     # Si l'utilisateur n'est pas le propriétaire, redirection avec message d'erreur
     unless @car.user == current_user
-      redirect_to cars_path, alert: "You are not authorized to edit this car."
+      redirect_to rentals_path(tab: "loueur"), alert: "You are not authorized to edit this car."
     end
   end
 
@@ -78,13 +80,13 @@ class CarsController < ApplicationController
     # Si l'utilisateur n'est pas le propriétaire, redirection avec message d'erreur
     # Le 'return' arrête l'exécution de la méthode immédiatement
     unless @car.user == current_user
-      return redirect_to cars_path, alert: "You are not authorized to edit this car."
+      return redirect_to rentals_path(tab: "loueur"), alert: "You are not authorized to edit this car."
     end
 
     # Tentative de mise à jour avec les paramètres filtrés
     if @car.update(car_params)
       # Redirection vers la page de détails de la voiture avec message de succès
-      redirect_to car_path(@car), notice: 'Car was successfully updated.'
+      redirect_to rentals_path(tab: "loueur"), notice: 'Car was successfully updated.'
     else
       # En cas d'échec, affichage du formulaire avec les erreurs
       flash.now[:alert] = "There were errors in your submission. Please check the form."
