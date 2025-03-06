b2bf1f4743d7392c36fad4f3666cd16ea2fe1c9e
Benjamin de Chazeaux <dechazeauxb@gmail.com>
2025-03-06 15:33:57 +0100

    Update New car

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,10 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
   static targets = ["togglableElement"]
+  
   connect() {
-    console.log("Hello from toggle_controller.js")
+    console.log("Toggle controller connected")
   }
-  fire() {
-    this.togglableElementTarget.classList.toggle("d-none");
+  
+  fire(event) {
+    event.preventDefault()
+    console.log("Toggle fire action triggered")
+    this.togglableElementTarget.classList.toggle("d-none")
   }
 }
